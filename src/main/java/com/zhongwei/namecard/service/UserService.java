5a89d5b9a6b49993979465e3a67fbfe6a76229d2
Administrator <Administrator@windows10.microdone.cn>
2019-02-28 16:45:38 +0800

    44

--- src/main/java/com/zhongwei/namecard/service/UserService.java
+++ src/main/java/com/zhongwei/namecard/service/UserService.java
@@ -1,5 +1,16 @@
 package com.zhongwei.namecard.service;
 
-public class UserService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.zhongwei.namecard.entity.UserDetailsEntity;
+
+public class UserService implements UserDetailsService{
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return new UserDetailsEntity();
+	}
 
 }
