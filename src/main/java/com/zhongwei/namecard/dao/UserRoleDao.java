25f289b65f91ca61a6d6de7ef427c4358b0303ac
yuhongtaoit <yuhongtaoit@126.com>
2019-02-28 20:26:30 +0800

    55

---
+++ src/main/java/com/zhongwei/namecard/dao/UserRoleDao.java
@@ -0,0 +1,22 @@
+package com.zhongwei.namecard.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.zhongwei.namecard.entity.RoleEntity;
+
+@Mapper
+public interface UserRoleDao {
+	
+	List<RoleEntity> getAll();
+	
+	RoleEntity getOne(Long id);
+
+	void insert(RoleEntity role);
+
+	void update(RoleEntity role);
+
+	void delete(Long id);
+
+}
