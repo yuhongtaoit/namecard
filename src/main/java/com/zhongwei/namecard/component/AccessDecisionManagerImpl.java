f911e1ebb008b69a19c9aa5c41591e7c033b70f4
Administrator <Administrator@windows10.microdone.cn>
2019-03-04 17:12:03 +0800

    99

--- src/main/java/com/zhongwei/namecard/component/AccessDecisionManagerImpl.java
+++ src/main/java/com/zhongwei/namecard/component/AccessDecisionManagerImpl.java
@@ -7,7 +7,9 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AccessDecisionManagerImpl implements AccessDecisionManager{
 
 	@Override
