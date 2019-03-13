994ecd547b87cdee89cb0f9826135d9c85912995
yuhongtaoit <yuhongtaoit@126.com>
2019-03-13 21:00:03 +0800

    33

--- src/main/java/com/zhongwei/namecard/controller/NameCardController.java
+++ src/main/java/com/zhongwei/namecard/controller/NameCardController.java
@@ -2,6 +2,9 @@ package com.zhongwei.namecard.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,8 +23,20 @@ public class NameCardController {
 	
 	@RequestMapping("/getNamecardList")
 	public String getUsers(Model model) {
-//		List<Card> cardList = cardMapper.selectByExample(new CardExample());
+		List<Card> cardList = cardMapper.selectByExample(new CardExample());
+		model.addAttribute("cards", cardList);
 		return "cardlist";
+	}
+	
+	@RequestMapping("/edit")
+	public String editCard(HttpServletRequest request, HttpServletResponse response, Model model){
+		String idStr = request.getParameter("id");
+		if(idStr!=null && idStr.trim().length()>0){
+			int id = Integer.valueOf(idStr);
+			Card card = cardMapper.selectByPrimaryKey(id);
+			model.addAttribute("card", card);
+		}
+		return "cardedit";
 	}
 	
 }
