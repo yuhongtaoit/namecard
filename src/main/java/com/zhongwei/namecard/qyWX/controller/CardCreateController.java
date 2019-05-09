24c1e757e1a7a29ac7b54201d93bfa6aea2a4071
渠天首 <qutianshou@163.com>
2019-05-09 13:56:16 +0800

    新增名片弹不出选择照片问题

--- src/main/java/com/zhongwei/namecard/qyWX/controller/CardCreateController.java
+++ src/main/java/com/zhongwei/namecard/qyWX/controller/CardCreateController.java
@@ -76,7 +76,8 @@ public class CardCreateController {
 		}else {
 			http_type = "http://";
 		}
-		String url = http_type + request.getServerName() + ":" + request.getServerPort() + "/cardCreate/cardCreate?uniacid=" + uniacid;
+//		String url = http_type + request.getServerName() + ":" + request.getServerPort() + "/cardCreate/cardCreate?uniacid=" + uniacid;
+		String url = http_type + request.getServerName()  + "/cardCreate/cardCreate?uniacid=" + uniacid;
 		Map<String, Object> Sign = QySendUtils.addSign(setQY.getCorpid(), ticket, url);
 		model.addAttribute("Sign", Sign);
 		model.addAttribute("projectRootPath", wxMaProperties.getProjectRootPath());
