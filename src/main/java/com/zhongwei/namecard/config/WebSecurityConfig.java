353f0aa226cd7894335036af8658fa64077fba54
LilyYu <yuhongtaoit@126.com>
2019-05-09 14:02:53 +0800

    域名校验文件

--- src/main/java/com/zhongwei/namecard/config/WebSecurityConfig.java
+++ src/main/java/com/zhongwei/namecard/config/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     //在这里配置哪些页面不需要认证
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/", "/miniapp/**", "/error/**", "/staffer/**","/member/**","/interact/**","/client/**", "/friend/**", "/home/**", "/cardCreate/**", 
+        web.ignoring().antMatchers("/", "/WW_verify_CwggVO7psd6hclMm.txt", "/miniapp/**", "/error/**", "/staffer/**","/member/**","/interact/**","/client/**", "/friend/**", "/home/**", "/cardCreate/**", 
         		"/qyImages/**","/css/**","/js/**","/jquery/**","/bootstrap/**","/images/**","/attachment/**", "/js/**" , "/ueditor/**" ,"/favicon.ico");
     }
  
